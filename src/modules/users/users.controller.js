6da467083c05084b28d852797a946d0b6b690217
Julsperez <julsperez@outlook.com>
2026-05-14 20:19:19 -0600

    Fixed controller backend

--- src/modules/users/users.controller.js
+++ src/modules/users/users.controller.js
@@ -3,5 +3,5 @@ import { UsersService } from './users.service.js'
 const usersService = new UsersService()
 
 export async function getMeHandler(request, reply) {
-  return reply.send(await usersService.getProfile(request.user.id))
+  return reply.send(await usersService.getProfile(request.user.userId))
 }
